0dda8171b89e4c6d241363a443e8f879a82f1f1c
whosvgogh <133163414+whosvgogh@users.noreply.github.com>
2023-10-26 16:37:43 -0300

    Add files via upload

---
+++ Calculos.js
@@ -0,0 +1,11 @@
+function multiplica() {
+    num1 = document.getElementById("n1").value;
+    num2 = document.getElementById("n2").value;
+    document.getElementById("resultado").innerHTML = num1 * num2;
+}
+
+function divide() {
+    num1 = document.getElementById("n1").value;
+    num2 = document.getElementById("n2").value;
+    document.getElementById("resultado").innerHTML = num1 / num2;
+}
